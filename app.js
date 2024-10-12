250e0500af4e5b122c3544c5be50e0f9c4b45d42
LanTuo <1264348626@qq.com>
2024-10-12 10:49:15 +0800

    服务器配置

--- app.js
+++ app.js
@@ -4,37 +4,47 @@ const loginRouter = require("./routers/login");
 const registerRouter = require("./routers/register");
 const userInfoRouter = require("./routers/userInfo");
 const orderRouter = require("./routers/order");
+const cors = require('cors');
 
 const app = express();
 
-app.all('*', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  //res.header('Access-Control-Allow-Headers',"token", 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By", ' 3.2.1')
-  res.header("Content-Type", "application/json;charset=utf-8");
-  next();
-});
+// CORS 配置，允许多个来源
+const allowedOrigins = ['http://localhost:5173', 'http://175.178.215.129'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // 如果请求来源在 allowedOrigins 列表中，或者没有来源（例如本地开发时的非浏览器请求），则允许
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true  // 如果你需要支持 cookie 等凭据
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api", loginRouter); // 注入登录路由模块
+// 路由配置
+app.use("/api", loginRouter);
 app.use("/api", registerRouter);
 app.use("/api", userInfoRouter);
 app.use("/api", orderRouter);
 
+// 启动服务器
 app.listen('8889', '0.0.0.0', () => {
-  console.log("启动成功")
-})
+  console.log("服务器启动成功，监听端口 8889");
+});
 
-// 连接错误
+// 数据库连接错误处理
 mongoose.connection.on('error', (err) => {
   console.log('数据库连接错误:', err);
 });
 
-// 连接成功
+// 数据库连接成功处理
 mongoose.connection.on('connected', () => {
   console.log('数据库连接成功！');
 });
